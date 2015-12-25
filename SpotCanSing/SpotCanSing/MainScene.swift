a256a280a2e21fe7708d730d1cee1bf39fa45728
Fritz Huie <fritz@Fritz.local>
2015-12-24 22:57:44 -0800

    yi is a genius

--- SpotCanSing/SpotCanSing/MainScene.swift
+++ SpotCanSing/SpotCanSing/MainScene.swift
@@ -9,6 +9,8 @@
 import UIKit
 import SpriteKit
 
+var requiredParts = [String:[String:[String]]]()
+
 class MainScene: SKScene {
     
     var frameCount = 0
@@ -16,7 +18,6 @@ class MainScene: SKScene {
     
     var availableMembers = 10
     var solos = [String:[String]]()
-    var requiredParts = [String:[String:[String]]]()
     var songsWeCanSing = [String]()
     let buttonsv = 5 //number of buttons per row
     var buttonSize = CGFloat()
@@ -54,7 +55,6 @@ class MainScene: SKScene {
     }
     
     func availableSongs ()->[String] {
-        //return an array of song names that have all parts covered
         
         var singable = [String]()
         
@@ -64,30 +64,60 @@ class MainScene: SKScene {
             }
             return singable
         }
-        
-        for song in requiredParts.keys {
+    
+        for s in requiredParts.keys { //for each song
+            let song = requiredParts[s]
             
-            var availableMembers = [String:Bool]()
-            var numberOfPartsRemaining = requiredParts[song]!.keys.count
-            for name in members.keys{availableMembers[name] = members[name]}
-            
-            for parts in requiredParts[song]!.keys {
-                var partCovered = false
-                for person in requiredParts[song]![parts]! {
-                    //iterate over every person in the song parts
-                    if (members[person] == true) {
-                        //check if that part has any people covering it
-                        partCovered = true
+            //build remaining part distribution dictionary
+            var dist = [String:[String]]()
+            for part in song!.keys {
+                for person in song![part]! {
+                    if (dist.keys.contains(person)) {
+                        dist[person]!.append(part)
+                    }else if (members[person] == true){
+                        dist[person] = [part]
                     }
                 }
-                if (partCovered) {
-                    numberOfPartsRemaining--
+            }
+            
+            let parts = Array(requiredParts[s]!.keys)
+            let people = Array(dist.keys)
+            for p in members.keys {
+                
+            }
+            
+            func checkForPartMatches(remainingParts: [String], remainingPeople: [String])->Bool
+            {
+                if(remainingParts.count == 0){
+                    return true
                 }
+                for person in remainingPeople {
+                    let part = remainingParts.first
+                    if (dist[person] != nil && dist[person]!.contains(part!)) {// if the current person sings this part
+                        var a = remainingPeople
+                        for(var i = 0; i < a.count; i++) {
+                            //remove person who matched
+                            if (a[i] == person){
+                                a.removeAtIndex(i)
+                            }
+                        }
+                        
+                        var b = remainingParts
+                        b.removeAtIndex(0)
+                        
+                        return checkForPartMatches(b, remainingPeople: a)
+                    }
+                }
+                print("\(s) not covered")
+                return false
             }
-            if (numberOfPartsRemaining == 0) {
-                singable.append(song)
+            
+            if(checkForPartMatches(parts, remainingPeople: people)){
+                singable.append(s)
             }
-        }
+            
+    }
+        
         return singable
     }
     
@@ -152,64 +182,21 @@ class MainScene: SKScene {
     }
     
     override func update(currentTime: CFTimeInterval) {
+        
+        //songlist fade in
         if (songListNode.alpha < 99.0) {
             var a = songListNode.alpha
             a = a + 0.07
             songListNode.alpha = a > 100.0 ? 100.0 : a
-            
-            for node in songListNode.children {
-                if (abs(node.position.x - frame.midX) > 5.0) {
-                    node.position.x = frame.midX + (node.position.x - frame.midX)/1.2
-                    node.position.x = node.position.x + (node.position.x > frame.midX ? -5.0 : 5.0)
-                }else{
-                    node.position.x = frame.midX
-                }
+        }
+        
+        //songlist sliding animation
+        for node in songListNode.children {
+            if (abs(node.position.x - frame.midX) > 5.0) {
+                node.position.x = frame.midX + (node.position.x - frame.midX)/1.3 + (node.position.x > frame.midX ? -5.0 : 5.0)
+            }else{
+                node.position.x = frame.midX
             }
         }
     }
-    
-    func generateSongList () {
-        
-        requiredParts["Always"] = [ "tenor": [chris, alan], "alto": [judy, caro], "bass": [alan, peter, bryce], "soprano":[angela, kristina, lisa], "solo":[fritz]]
-        
-        //songs below have no parts assigned
-        
-        requiredParts["Angel Without Wings"] = ["solo":[chris]]
-        requiredParts["Book of Love"] = ["solo":[bryce]]
-        requiredParts["Breathe"] = ["solo":[lisa]]
-        requiredParts["Can't Illuminate"] = ["solo":[kristina]]
-        requiredParts["Climbing Out Whole"] = ["solo":[judy, angela, caro]]
-        requiredParts["Feelin' Good"] = ["solo":[chris, bryce]]
-        requiredParts["House by the Sea"] = ["solo":[fritz, caro]]
-        requiredParts["Hymn of Axciom"] = ["solo":[judy]]
-        requiredParts["Intergalactic"] = ["solo":[bryce, lisa, alan]]
-        requiredParts["Istanbul"] = ["solo":[fritz, alan]]
-        requiredParts["Jesu, Annoying Chris's Desiring"] = ["solo":[""]]
-        requiredParts["Keep Breathing"] = ["solo":[judy]]
-        requiredParts["King of Spain"] = ["solo":[lisa]]
-        requiredParts["Kraken"] = ["solo":[""]]
-        requiredParts["La Grippe"] = ["solo":[fritz, chris, caro]]
-        requiredParts["Let's Get It Started"] = ["solo":[fritz, alan, angela]]
-        requiredParts["Milkshake Madrigal"] = ["solo":[""]]
-        requiredParts["Money"] = ["solo":[alan, bryce]]
-        requiredParts["Monster"] = ["solo":[angela]]
-        requiredParts["My Baby Loves a Bunch of Authors"] = ["solo":[lisa]]
-        requiredParts["Favorite Things"] = ["solo":[bryce]]
-        requiredParts["One Love"] = ["solo":[caro]]
-        requiredParts["Psychic"] = ["solo":[peter]]
-        requiredParts["Rainbow Connection"] = ["solo":[kristina, angela, chris]]
-        requiredParts["Russian Unicorn"] = ["solo":[fritz, alan]]
-        requiredParts["Sail"] = ["solo":[angela]]
-        requiredParts["Say Goodbye"] = ["solo":[angela, caro]]
-        requiredParts["She's Already Gone"] = ["solo":[caro]]
-        requiredParts["Show Me Your Patronus"] = ["solo":[caro]]
-        requiredParts["Sorry"] = ["solo":[lisa]]
-        requiredParts["Squirrel Samba"] = ["solo":[""]]
-        requiredParts["Star Phoenix Plum Jam"] = ["solo":[""]]
-        requiredParts["Stockholm Syndrom"] = ["solo":[judy]]
-        requiredParts["Sweet Caroline"] = ["solo":[lisa]]
-        requiredParts["Warning Sign"] = ["solo":[fritz]]
-    }
-    
-    let angela = "angela", alan = "alan", bryce = "bryce", caro = "caro", chris = "chris", fritz = "fritz", judy = "judy", kristina = "kristina", lisa = "lisa", peter = "peter"
 }
---
+++ SpotCanSing/SpotCanSing/Songlist.swift
@@ -0,0 +1,51 @@
+
+func generateSongList () {
+    
+    requiredParts["Always"] = [ "tenor": [chris, alan],
+        "alto": [judy, caro],
+        "bass": [alan, peter, bryce],
+        "soprano":[angela, kristina, lisa],
+        "solo":[fritz]]
+    
+    requiredParts["Angel Without Wings"] = ["solo":[chris], "tenor":[fritz, alan]]
+    requiredParts["Book of Love"] = ["solo":[bryce], "t":[fritz, chris], "t2":[chris], "b":[peter, alan], "a":[judy, lisa], "s":[angela, caro]]
+    
+    let testParts = [String:[String:[String]]]()
+    
+    
+    //        requiredParts["Breathe"] = ["solo":[lisa]]
+    //        requiredParts["Can't Illuminate"] = ["solo":[kristina]]
+    //        requiredParts["Climbing Out Whole"] = ["solo":[judy, angela, caro]]
+    //        requiredParts["Feelin' Good"] = ["solo":[chris, bryce]]
+    //        requiredParts["House by the Sea"] = ["solo":[fritz, caro]]
+    //        requiredParts["Hymn of Axciom"] = ["solo":[judy]]
+    //        requiredParts["Intergalactic"] = ["solo":[bryce, lisa, alan], "solo2": [kristina, angela]]
+    //        requiredParts["Istanbul"] = ["solo":[fritz, alan]]
+    //        requiredParts["Jesu, Annoying Chris's Desiring"] = ["solo":[""]]
+    //        requiredParts["Keep Breathing"] = ["solo":[judy]]
+    //        requiredParts["King of Spain"] = ["solo":[lisa]]
+    //        requiredParts["Kraken"] = ["solo":[""]]
+    //        requiredParts["La Grippe"] = ["solo":[fritz, chris, caro]]
+    //        requiredParts["Let's Get It Started"] = ["solo":[fritz, alan, angela]]
+    //        requiredParts["Milkshake Madrigal"] = ["solo":[""]]
+    //        requiredParts["Money"] = ["solo":[alan, bryce]]
+    //        requiredParts["Monster"] = ["solo":[angela]]
+    //        requiredParts["My Baby Loves a Bunch of Authors"] = ["solo":[lisa]]
+    //        requiredParts["Favorite Things"] = ["solo":[bryce]]
+    //        requiredParts["One Love"] = ["solo":[caro]]
+    //        requiredParts["Psychic"] = ["solo":[peter]]
+    //        requiredParts["Rainbow Connection"] = ["solo":[kristina, angela, chris]]
+    //        requiredParts["Russian Unicorn"] = ["solo":[fritz, alan]]
+    //        requiredParts["Sail"] = ["solo":[angela]]
+    //        requiredParts["Say Goodbye"] = ["solo":[angela, caro]]
+    //        requiredParts["She's Already Gone"] = ["solo":[caro]]
+    //        requiredParts["Show Me Your Patronus"] = ["solo":[caro]]
+    //        requiredParts["Sorry"] = ["solo":[lisa]]
+    //        requiredParts["Squirrel Samba"] = ["solo":[""]]
+    //        requiredParts["Star Phoenix Plum Jam"] = ["solo":[""]]
+    //        requiredParts["Stockholm Syndrom"] = ["solo":[judy]]
+    //        requiredParts["Sweet Caroline"] = ["solo":[lisa]]
+    //        requiredParts["Warning Sign"] = ["solo":[fritz]]
+}
+
+let angela = "angela", alan = "alan", bryce = "bryce", caro = "caro", chris = "chris", fritz = "fritz", judy = "judy", kristina = "kristina", lisa = "lisa", peter = "peter"
